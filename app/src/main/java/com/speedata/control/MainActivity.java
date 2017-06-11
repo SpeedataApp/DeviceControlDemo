bdba69cf331fb594a65182bb962f6cfb1a69483a
Reginer <282921012@qq.com>
2017-06-12 01:33:49 +0800

    first commit

---
+++ app/src/main/java/com/speedata/control/MainActivity.java
@@ -0,0 +1,102 @@
+package com.speedata.control;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.control.ControlManager;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ControlManager mControlManager;
+    private static final String TAG = "Reginer";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mControlManager = ControlManager.getDeviceControl(this);
+        Button mBtnReboot = (Button) findViewById(R.id.btn_reboot);
+        Button mBtnShutdown = (Button) findViewById(R.id.btn_shutdown);
+        Button mBtnRecovery = (Button) findViewById(R.id.btn_recovery);
+        mBtnReboot.setOnClickListener(this);
+        mBtnShutdown.setOnClickListener(this);
+        mBtnRecovery.setOnClickListener(this);
+        Button mBtnSetTime = (Button) findViewById(R.id.btn_set_time);
+        mBtnSetTime.setOnClickListener(this);
+        Button mBtnCreateApn = (Button) findViewById(R.id.btn_create_apn);
+        mBtnCreateApn.setOnClickListener(this);
+        Button mBtnUninstall = (Button) findViewById(R.id.btn_uninstall);
+        mBtnUninstall.setOnClickListener(this);
+        Button mBtnInstall = (Button) findViewById(R.id.btn_install);
+        mBtnInstall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_reboot:
+                mControlManager.rebootDevice();
+                break;
+            case R.id.btn_shutdown:
+                mControlManager.shutdownDevice();
+                break;
+            case R.id.btn_recovery:
+                mControlManager.recoveryDevice();
+                break;
+            case R.id.btn_set_time:
+                mControlManager.setSystemTime(1497369600);
+                break;
+            case R.id.btn_create_apn:
+                int apnId;
+                apnId = mControlManager.createApn(createApn());
+                Log.d(TAG, "apnId  is: " + apnId);
+                break;
+            case R.id.btn_uninstall:
+                mControlManager.uninstallApp("win.reginer.life");
+                break;
+            case R.id.btn_install:
+                String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+                mControlManager.installApp(Uri.fromFile(new File(path+File.separator+"DeviceControl.apk")));
+                break;
+        }
+    }
+
+    private ContentValues createApn() {
+        String NUMERIC;
+        TelephonyManager telephonyManager = (TelephonyManager)
+                getSystemService(Context.TELEPHONY_SERVICE);
+        NUMERIC = telephonyManager.getSimOperator();
+        String[] EM_APN = {"Speedata", "spd", "460", "02", "default,supl"};
+        ContentValues values = new ContentValues();
+        values.put("name", EM_APN[0]);
+        values.put("apn", EM_APN[1]);
+        values.put("type", EM_APN[4]);
+        values.put("numeric", NUMERIC);
+        values.put("mcc", "460");
+        values.put("mnc", "00");
+        values.put("proxy", "");
+        values.put("port", "");
+        values.put("mmsproxy", "");
+        values.put("mmsport", "");
+        values.put("user", "");
+        values.put("server", "");
+        values.put("password", "");
+        values.put("mmsc", "");
+        return values;
+    }
+}
